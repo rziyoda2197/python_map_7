69540f09d9a541ec0c3461f5aa12bd15ae36aa1f
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-17 16:48:30 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+sozlar = ["python", "map", "function", "lambda", "filter"]
+natija = list(map(lambda s: s[::-1].upper(), sozlar))
+print(natija)
+
+ismlar = ["ali", "vali", "sardor", "ziyoda", "madina"]
+natija = list(map(lambda s: s[::-1].upper(), ismlar))
+print(natija)
+
+shaharlar = ["toshkent", "samarqand", "buxoro", "andijon"]
+natija = list(map(lambda s: s[::-1].capitalize(), shaharlar))
+print(natija)
